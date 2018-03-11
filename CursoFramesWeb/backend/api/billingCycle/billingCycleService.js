1625b8ab1d50375d4dfdf92ff8874472c53fe14d
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-11 00:07:15 -0300

    Testando a API de Ciclo de Pagamentos 2

--- CursoFramesWeb/backend/api/billingCycle/billingCycleService.js
+++ CursoFramesWeb/backend/api/billingCycle/billingCycleService.js
@@ -4,5 +4,11 @@ const BillingCycle = require('./billingCycle')
 // Métodos que irão ser criados na API 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 
+// Configurações adicionais
+BillingCycle.updateOptions({ 
+    new: true,              // Ao alterar um registro é retornado o registro novo ao invés do antigo
+    runValidators: true    // Habilita as validações pro 'update'
+})
+
 // Exporta o módulo
 module.exports = BillingCycle
--- CursoFramesWeb/backend/config/database.js
+++ CursoFramesWeb/backend/config/database.js
@@ -4,5 +4,8 @@ const mongoose = require('mongoose')
 // Conecta com o banco de dados (MongoDB) e retorna para o 'module.exports'
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
-// Define uma mensagem de validação personalizada para os atributos 'required'
+// Define as mensagens de validação personalizadas
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínino de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."

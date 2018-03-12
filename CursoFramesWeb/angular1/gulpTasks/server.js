ade014449287b3e0e3a0f173610e5a839acac6ea
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-11 22:04:08 -0300

    Configurando o Servidor com Reload Automático

--- CursoFramesWeb/angular1/gulpTasks/server.js
+++ CursoFramesWeb/angular1/gulpTasks/server.js
@@ -1,12 +1,22 @@
-// Importa o 'gulp'
+// Importa as dependências
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 
 // Cria a task 'watch'
 gulp.task('watch', () => {
-
+    watch('app/**/*.html', () => gulp.start('app.html'))
+    watch('app/**/*.css', () => gulp.start('app.css'))
+    watch('app/**/*.js', () => gulp.start('app.js'))
+    watch('assets/**/*.*', () => gulp.start('app.assets'))
 })
 
 // Cria a task 'server'
 gulp.task('server', ['watch'], () => {
-    
+    return gulp.src('public')
+        .pipe(webserver({
+            livereload: true,
+            port: 3000,
+            open: true
+        }))
 })

50abf6bd26225a23be116f8306b8aabfae3e4246
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-18 23:23:07 -0300

    Dashboard Controller

---
+++ CursoFramesWeb/angular1/app/dashboard/dashboardController.js
@@ -0,0 +1,19 @@
+// Cria o controller do dashboard
+angular.module('primeiraApp').controller('DashboardCtrl', [
+    '$scope',
+    '$http',
+    DashboardController
+])
+
+function DashboardController($scope, $http) {
+    $scope.getSummary = function() {
+        const url = 'http://localhost:3003/api/billingSummary'
+        $http.get(url).then(function({ credit = 0, debt = 0 }) {
+            $scope.credit = credit
+            $scope.debt = debt
+            $scope.total = credit - debt
+        })
+    }
+
+    $scope.getSummary()
+}

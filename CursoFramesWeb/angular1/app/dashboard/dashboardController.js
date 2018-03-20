46ba0b62ce99f86bcb1a2936b9de9ccc4eba7745
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-19 22:07:02 -0300

    Habilitando o CORS na Aplicação Backend

--- CursoFramesWeb/angular1/app/dashboard/dashboardController.js
+++ CursoFramesWeb/angular1/app/dashboard/dashboardController.js
@@ -8,7 +8,8 @@ angular.module('primeiraApp').controller('DashboardCtrl', [
 function DashboardController($scope, $http) {
     $scope.getSummary = function() {
         const url = 'http://localhost:3003/api/billingSummary'
-        $http.get(url).then(function({ credit = 0, debt = 0 }) {
+        $http.get(url).then(function(response) {
+            const { credit = 0, debt = 0 } = response.data
             $scope.credit = credit
             $scope.debt = debt
             $scope.total = credit - debt
--- CursoFramesWeb/backend/config/server.js
+++ CursoFramesWeb/backend/config/server.js
@@ -6,11 +6,15 @@ const bodyParser = require('body-parser')
 const express = require('express')
 // Cria o servidor
 const server = express()
+// Define as configurações de CORS
+const allowCors = require('./cors')
 
 // Adiona o 'bodyParser.urlencoded' para todas requisições vindas de formulários
 server.use(bodyParser.urlencoded({ extended: true }))
 // Adiona o 'bodyParser.json' para todas requisições vindas como JSON
 server.use(bodyParser.json())
+// Adiona o 'CORS' para todas requisições
+server.use(allowCors)
 
 // Inicializa o servidor
 server.listen(port, () => console.log(`BACKEND is running on port ${port}.`))

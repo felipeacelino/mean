e0775a005d9a510b4d1cce7310f443d8c09dab2c
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-22 00:40:02 -0300

    Atualizando o BillingCycleController para Angular 1.6

--- CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
+++ CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
@@ -8,7 +8,7 @@
         const vm = this
         vm.create = function() {
             const url = 'http://localhost:3003/api/billingCycles'
-            $http.post(url, vm.billingCycle).success(function(response) {
+            $http.post(url, vm.billingCycle).then(function(response) {
                 vm.billingCycle = {}
                 console.log('Sucesso!')
             })

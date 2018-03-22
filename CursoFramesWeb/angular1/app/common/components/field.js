16d7593e42dc49cacde660da60d1a0b4cef9d3bb
Felipe Acelino <felipeacelino@hotmail.com>
2018-03-22 00:32:30 -0300

    Versão Inicial do Controller de Ciclo de Pagamentos

--- CursoFramesWeb/angular1/app/common/components/field.js
+++ CursoFramesWeb/angular1/app/common/components/field.js
@@ -6,7 +6,8 @@
             label: '@',
             grid: '@',
             placeholder: '@',
-            type: '@'   
+            type: '@',
+            model: '='
         },
         controller: [
             'gridSystem',
@@ -18,7 +19,7 @@
         <div class="{{ $ctrl.gridClasses }}">
             <div class="form-group">
                 <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-                <input type="{{ $ctrl.type }}" id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" />
+                <input type="{{ $ctrl.type }}" id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" ng-model="$ctrl.model" />
             </div>
         </div>
         `
